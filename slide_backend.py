e3e1f8c3fb5e5ff2c2f068687cc991fc044ee0ea
Chriscodinglife <chrisdotorellana@gmail.com>
2022-10-04 20:26:51 -0400

    created the slide_backend

---
+++ slide_backend.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from slider import Slider
+
+app = FastAPI()
+
+slider = Slider()
+
+@app.get("/ping", status_code=200)
+def ping():
+    '''
+    Return a generic ping response
+    '''
+    return {"Hello": "World"}
+
+
+@app.get("/total_slides", status_code=200)
+def total_slides():
+    '''
+    Return the total number of slides in the presentation
+    '''
+    slides = slider.get_number_of_slides()
+    return {"total_slides": len(slides)}
+
+
+@app.get("/generate_slides", status_code=200)
+def generate_slides():
+    '''
+    Generate new slides upon request and return a success message
+    '''
+    slide_status = slider.run()
